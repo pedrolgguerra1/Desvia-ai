39a52c912fc3ae9ea5fc50129b27bf6f9c6fa19e
Pedro <pguerrape@gmail.com>
2025-05-21 11:46:55 -0300

    adição das bibliotecas e dimensões

---
+++ main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include "raylib.h"
+#include <stdlib.h>
+#include <time.h>
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+#define PLAYER_WIDTH 50
+#define PLAYER_HEIGHT 20
+#define ENEMY_SIZE 20
+#define MAX_ENEMIES 10
